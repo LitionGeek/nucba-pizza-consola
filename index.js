9f26f96ece7ec4fafddf36f1bf3d678d4a51bf2c
Your Name <you@example.com>
2022-04-01 09:59:46 -0300

    Se resube el ejercicio E1

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const recorrerIngredientes = () =>{
     let ingredietesImPares=[];
 
    ingredientes.forEach(ingrediente=>{
-       if(ingrediente.length% 2 ===0){
+       if(ingrediente.length% 2 ==0){
             ingredietesPares.push(ingrediente);
        }else{
             ingredietesImPares.push(ingrediente);
